466441adfff4a3ec8bfe0f5e18c13dac97536295
bschiranth <bschiranth@gmail.com>
2019-08-04 13:26:34 -0500

    adding about menu item

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -2,18 +2,21 @@ package com.example.android.navigation
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 
 class TitleFragment : Fragment() {
+
+    lateinit var navigationController: NavController
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -21,16 +24,30 @@ class TitleFragment : Fragment() {
 
         val fragmentBinding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false);
 
+        navigationController = findNavController()
+
         fragmentBinding.playButton.setOnClickListener { view ->
-            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+            //view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+            navigationController.navigate(R.id.action_titleFragment_to_gameFragment)
         }
 
         //can also do navigation this way
         /*fragmentBinding.playButton.setOnClickListener (
             Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         )*/
+
+        setHasOptionsMenu(true)
+
         return fragmentBinding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!, navigationController) || super.onOptionsItemSelected(item)
+    }
 
 }
